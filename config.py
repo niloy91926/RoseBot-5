71b3c600273103be8269788f1a72b4b4157824f9
niloy91926 <niloyahmednil987@gmail.com>
2026-07-09 04:45:42 +0600

    Fix config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import os
+	mport os
 
-BOT_TOKEN = os.environ["8818235376:AAHlcs5h837N0hjSK9jPTR6xjgNwgaFGMrM"]
-API_ID = int(os.environ["38175655"])
-API_HASH = os.environ["540f6f3b92df426ea57842c4e60f073a"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
