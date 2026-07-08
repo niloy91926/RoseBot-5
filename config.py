e75a5047a613e26feacdd7df06cfa93e35147d75
niloy91926 <niloyahmednil987@gmail.com>
2026-07-09 04:38:13 +0600

    Fix config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.getenv("38175655"))
-API_HASH = os.getenv("540f6f3b92df426ea57842c4e60f073a")
-BOT_TOKEN = os.getenv("8818235376:AAHlcs5h837N0hjSK9jPTR6xjgNwgaFGMrM")
+BOT_TOKEN = os.environ["8818235376:AAHlcs5h837N0hjSK9jPTR6xjgNwgaFGMrM"]
+API_ID = int(os.environ["38175655"])
+API_HASH = os.environ["540f6f3b92df426ea57842c4e60f073a"]
